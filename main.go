3e56ddb1f04c6bcd9a9b7b8624757d66e80f1e88
NoobForAl <58856931+NoobforAl@users.noreply.github.com>
2023-11-02 17:00:29 +0330

    fix import file & name variable
    Signed-off-by: NoobForAl <58856931+NoobforAl@users.noreply.github.com>

--- main.go
+++ main.go
@@ -2,13 +2,14 @@ package main
 
 import (
 	"web_proxy/config"
-	"web_proxy/core/logger"
 	server "web_proxy/http"
+	"web_proxy/logger"
 )
 
 func main() {
 	conf := config.Arg
 	log := logger.New(conf.Debug, conf.LogFile)
-	serv := server.New(log, conf)
-	log.Fatal(serv.Run(config.Arg.Listen))
+
+	serve := server.New(log, conf)
+	log.Fatal(serve.Run(config.Arg.Listen))
 }
